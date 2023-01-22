31261385535d4e52282070e5544cafffed2b9654
Andrey Pakhomov <pahomeathome@gmail.com>
2023-01-22 19:49:34 +0200

    styles for task 4 added

--- task4.js
+++ task4.js
@@ -14,19 +14,32 @@ const createBlocks = (number) => {
     const block = document.createElement(`div`);
     block.style.backgroundColor = `${color}`;
     block.style.outline = "1px solid black";
-    block.innerHTML = `${i}`;
+    block.innerHTML = `${0}`;
     block.style.display = "flex";
     block.style.gridArea = template[i - 1];
 
     if (i === 1) {
-      block.style.backgroundColor = `red`;
-      // block.style.gridRow = "1 / 4";
+      block.style.backgroundColor = `rgba(254,0,0,255)`;
+    }
+
+    if (i === 2) {
+      block.style.backgroundColor = `rgba(92,0,254,255)`;
+    }
+
+    if (i === 3) {
+      block.style.backgroundColor = `rgba(0,142,0,255)`;
     }
 
     if (i === 4) {
-      block.style.backgroundColor = `purple`;
-      // block.style.gridColumn = "2 / 4";
-      // block.style.gridRow = "2 / 3";
+      block.style.backgroundColor = `rgba(143,0,130,255)`;
+    }
+
+    if (i === 5) {
+      block.style.backgroundColor = `rgba(210,134,50,255)`;
+    }
+
+    if (i === 6) {
+      block.style.backgroundColor = `rgba(232,241,168,255)`;
     }
 
     block.style.justifyContent = "center";
@@ -38,6 +51,9 @@ const createBlocks = (number) => {
 
 const commonBlock = document.createElement(`div`);
 commonBlock.style.display = "grid";
+commonBlock.style.fontSize = "22px";
+commonBlock.style.fontFamily = "Arial";
+commonBlock.style.fontWeight = "700";
 commonBlock.style.gridTemplateAreas = `"a b c" "a d d" "a d d" "a d d" "a d d" "a d d" "a d d" "a d d" "a d d" "a e f"`;
 commonBlock.style.width = "90vw";
 commonBlock.style.height = "33vh";
