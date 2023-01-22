57307ec90c86f2503c882c2561403b2c2935daa0
Andrey Pakhomov <pahomeathome@gmail.com>
2023-01-22 08:46:54 +0200

    task 3 completed

---
+++ task3.js
@@ -0,0 +1,36 @@
+const bar = (length) => {
+  const createArray = (length) => new Array(length).fill(0);
+  const addNumbers = (array, index) => {
+    array.fill(2, 0 + index, length - index);
+    array.fill(2, length - (index + 1), 0 + (index + 1));
+    array.fill(1, 0 + (index + 1), length - (index + 1));
+    array.fill(1, length - index, 0 + index);
+  };
+
+  const array = createArray(length);
+  array.forEach((arr, index) => {
+    array[index] = createArray(length);
+    addNumbers(array[index], index);
+  });
+
+  return array;
+};
+
+console.log(bar(5));
+/*[
+[2,1,1,1,2],
+[0,2,1,2,0],
+[0,0,2,0,0],
+[0,2,1,2,0],
+[2,1,1,1,2],
+]
+*/
+
+console.log(bar(4));
+/*[
+[2,1,1,2],
+[0,2,2,0],
+[0,2,2,0],
+[2,1,1,2],
+]
+*/
